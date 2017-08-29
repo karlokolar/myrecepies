89844ebbd9b6bd8e9aee3d1408392a2e8bbe6756
Karlo Kolar <alien@kolar.link>
2017-08-29 18:26:29 +0200

    New, edit, create and update for chef

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: {:show, :edit, :update }
+  before_action :set_recipe, only: [ :show, :edit, :update ]
   def index
     @recipes = Recipe.all
   end
