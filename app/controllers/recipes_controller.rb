e285bd63566cbc21803f3313667a40ce107220b5
Karlo Kolar <alien@kolar.link>
2017-08-30 11:51:54 +0200

    Complete authetication system

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,8 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: [ :show, :edit, :update ]
+  before_action :set_recipe, only: [ :show, :edit, :update, :destroy ]
+  before_action :require_user, except: [ :index, :show ]
+  before_action :require_same_user, only: [ :edit, :update, :destroy ]
+
   def index
     @recipes = Recipe.paginate(page: params[:page], per_page: 5)
   end
@@ -14,7 +17,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
-    @recipe.chef = Chef.first
+    @recipe.chef = current_chef
     if @recipe.save
       flash[:success] = "The recipe was saved!"
       redirect_to recipe_path(@recipe)
@@ -48,5 +51,11 @@ class RecipesController < ApplicationController
     end
     def set_recipe
       @recipe = Recipe.find(params[:id])
+    end
+    def require_same_user
+      if current_chef != @recipe.chef
+        flash[:danger] = "You can only edit or delete you own recipes!"
+        redirect_to recipes_path
+      end
     end
 end
